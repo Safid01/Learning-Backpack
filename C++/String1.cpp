082cdafb2e3f61e8026a2e6ca86b6bf815760eb7
Safid01 <safidh20@gmail.com>
2024-11-18 10:38:25 +0600

    Class Practice

---
+++ C++/String1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    char str[]={'S','a','f','i','d'};
+    int n = sizeof(str)/sizeof(str[0]);
+    for (int i=0; i<n; i++)
+     {
+        cout<<str[i];
+     }
+     cout<<endl;
+     cout<<str<<endl;
+     
+     char str1[] = "Safid Hasan";
+     cout<<str1,,endl;
+return 0;
+
+}
