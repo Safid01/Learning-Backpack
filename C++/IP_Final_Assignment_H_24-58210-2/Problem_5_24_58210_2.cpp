99600a275264ab9616779c864c8b7cc9a94a6a7e
Safid01 <safidh20@gmail.com>
2025-01-19 02:34:13 +0600

    Final assignment

--- C++/IP_Final_Assignment_H_24-58210-2/Problem_5_24_58210_2.cpp
+++ C++/IP_Final_Assignment_H_24-58210-2/Problem_5_24_58210_2.cpp
@@ -1,18 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int years[5];
-    cout << "Enter 5 years: ";
-    for (int i = 0; i < 5; i++) {
-        cin >> years[i];
-    }
-    cout << "Leap years: ";
-    for (int i = 0; i < 5; i++) {
-        int year = years[i];
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-        cout << year <<" ";
+void modifyArray(int *arr, int n) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            arr[i] += 3;
+        } else {
+            arr[i] *= 2;
         }
     }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[100]; // Fixed size array
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Original Array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    modifyArray(arr, n);
+
+    cout << "Modified Array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
--- C++/IP_Final_Assignment_H_24-58210-2/Problem_6_24_58210_2.cpp
+++ C++/IP_Final_Assignment_H_24-58210-2/Problem_6_24_58210_2.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
+struct Point {
+    int x, y;
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= i; j++) {
-            cout << j;
+    int distance(Point p) {
+        int dx = (p.x - x) * (p.x - x);
+        int dy = (p.y - y) * (p.y - y);
+        int dist = 0;
+        for (int i = 1; i <= dx + dy; i++) {
+            if (i * i == dx + dy) {
+                dist = i;
+                break;
+            }
         }
-        cout << endl;
+        return dist;
+    }
+
+    bool isCollinear(Point p1, Point p2) {
+        return (p2.y - p1.y) * (p1.x - x) == (p1.y - y) * (p2.x - p1.x);
+    }
+};
+int main() {
+    Point p1, p2, p3;
+
+    cout << "Enter coordinates of Point 1 (x1 y1): ";
+    cin >> p1.x >> p1.y;
+
+    cout << "Enter coordinates of Point 2 (x2 y2): ";
+    cin >> p2.x >> p2.y;
+
+    cout << "Distance between Point 1 and Point 2: " << p1.distance(p2) << endl;
+
+    cout << "Enter coordinates of Point 3 (x3 y3): ";
+    cin >> p3.x >> p3.y;
+
+    if (p3.isCollinear(p1, p2)) {
+        cout << "Point 3 lies on the line formed by Point 1 and Point 2." << endl;
+    } else {
+        cout << "Point 3 does not lie on the line formed by Point 1 and Point 2." << endl;
     }
     return 0;
 }
